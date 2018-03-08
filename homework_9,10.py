65b7ef4e7d6895983f446693f42d8cf4b9cf82a4
Aleksey <alexeyudin1982@gmail.com>
2018-03-08 17:42:43 +0200

    Сделал №9 и №10 и просьба посмотреть №7 т.к стоит не выполненно

--- homework_9,10.py
+++ homework_9,10.py
@@ -1,8 +1,17 @@
+#9
 
 str1 = 'alerksey_yudin_olegovisch'
 lst = str1.split('_')
 print(str1, '->', (lst[0].title() + lst[1].title() + lst[2].title()))
 
+#10
 
-
-
+str2 = 'Artur Pirojkov*1944-08-28*2010-11-20'
+lst1 = str2.split('*')
+name = lst1[0]
+lst2 = lst1[1]
+lst3 = lst2.split('-')
+lst4 = lst1[2]
+lst5 = lst4.split('-')
+year_of_life = ((int(lst5[0])) - (int(lst3[0])))
+print(name, year_of_life)
