f9987e463ba4db99e97a5a7c321d3de6b6b61c2b
Aleksey <alexeyudin1982@gmail.com>
2018-03-13 09:51:39 +0200

    Исправил №7

--- homework_7_8.py
+++ homework_7_8.py
@@ -3,10 +3,10 @@ date = '05.17.2016'
 month = int(date[:2])
 day = int(date[3:5])
 year = int(date[6:])
-date = ("American date: %d. %d. %d." % (month, day, year))
-date1 = ("European date: %d. %d. %d. " % (day, month, year))
-print(date)
+date1 = ("American date: %d. %d. %d" % (month, day, year))
+date2 = ("European date: %d. %d. %d" % (day, month, year))
 print(date1)
+print(date2)
 
 # 8
 name = 'Aleksey Yudin'
