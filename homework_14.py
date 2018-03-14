7361d3fde689d1b60cc0c385cab32b387c5754df
Aleksey <alexeyudin1982@gmail.com>
2018-03-14 12:43:29 +0200

    !

---
+++ homework_14.py
@@ -0,0 +1,9 @@
+def is_even():
+    number = int(input("Введите число: "))
+    if number % 2 > 0:
+        print("Не чётное число")
+        if number % 2 < 0:
+            print("Не чётное число")
+    return number
+print(is_even())
+
