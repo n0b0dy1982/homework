d64852ed45e313f34c5867add26c0735e9093764
Aleksey <alexeyudin1982@gmail.com>
2018-03-27 15:54:06 +0300

    Исправил

--- homework_17.py
+++ homework_17.py
@@ -1,16 +1,17 @@
 import math
 def solve_quadratic_equation(a, b, c):
-    D = b**2 - 4 * a * c
+    D = b**2 - (4 * a * c)
     if D > 0:
-        x1 = - b + math.sqrt(D) / (2 * a)
-        x2 = - b - math.sqrt(D) / (2 * a)
+        x1 = (- b + math.sqrt(D)) / (2 * a)
+        x2 = (- b - math.sqrt(D)) / (2 * a)
         return x1, x2
     if D == 0:
         x = - b / (2 * a)
         return x, None
     else:
         return None, None
-print(solve_quadratic_equation(1, 2, 1))
+print(solve_quadratic_equation(10, 2, 0))
+
 
 
 
